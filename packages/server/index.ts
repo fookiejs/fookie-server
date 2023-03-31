7d1201404c9a9bdab6488ade4b70f2b682107b1e
umudik <seyitumutbicer@gmail.com>
2023-03-31 03:57:03 +0300

    dev

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -3,6 +3,7 @@ import f from "fastify"
 const fastify = f()
 
 export const listen = async function (port, fookie) {
+    console.log("Server is running on port:", port)
     fastify.post("/", async (request) => {
         return await fookie.run(request.body)
     })
