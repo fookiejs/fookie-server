db6164c7c2dde24fafde7bb93dbf8f5c2f3a9909
umudik <seyitumutbicer@gmail.com>
2023-03-31 03:37:41 +0300

    dev

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,14 +1,14 @@
-import fastify from "fastify"
+import f from "fastify"
 
-const f = fastify()
+const fastify = f()
 
 export const listen = async function (port, fookie) {
-    f.post("/", async (request) => {
+    fastify.post("/", async (request) => {
         return await fookie.run(request.body)
     })
 
     try {
-        await f.listen({ port: port })
+        await fastify.listen({ port: port })
     } catch (err) {
         process.exit(1)
     }
